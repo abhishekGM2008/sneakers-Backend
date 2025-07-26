c32cf2eabec7c49afba6c21304040868893a94a9
abhihskeGM2008 <abhishek.gmbuj07@gmail.com>
2025-07-26 13:27:20 +0530

    updated

--- models/products.models.js
+++ models/products.models.js
@@ -4,18 +4,36 @@ const productsSchema = new mongoose.Schema({
    title: {
     type: String,
    } ,
-   tagline: String,
-   price:Number,
-   productImage: String,
+   tagline: {
+      type: String
+   },
+   price:{
+      type: Number
+   },
+   productImage: {
+      type: String
+   },
    category: {
     type: String,
     enum: ["Men", "Unisex"]
    },
-   details: String,
-   color: String,
-   style: String,
-   origin: String
-}, {timestamps:true}
-)
-const Sneakers = mongoose.model("Sneakers", productsSchema)
-module.exports = Sneakers
+   details: {
+    type: String
+   },
+   color: {
+      type: String
+   },
+   style: {
+      type: String
+   },
+   origin: {
+      type: String
+   },
+   quantity: {
+      type:Number,
+      default: 0
+   }
+});
+
+const Sneakers = mongoose.model("Sneakers", productsSchema);
+module.exports = Sneakers;
